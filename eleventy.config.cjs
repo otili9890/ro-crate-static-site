c2493e3c82f6991bb832671b4862294d87f99941
Peter Sefton <pt@ptsefton.com>
2024-08-11 07:20:35 +1000

    Running now with some config hardwired into index.js

--- eleventy.config.cjs
+++ eleventy.config.cjs
@@ -9,7 +9,7 @@ module.exports = function (eleventyConfig) {
 
 
       dir : {
-        input: "src",
+        input: "tmp",
         output: "dist",
         includes: "../_includes",
       }
--- index.js
+++ index.js
@@ -11,7 +11,16 @@ import * as Path from "path"
 // Creates a bunch of Markown files for an RO-Crate for 11ty or other static site generators to process
 
 // TODO - write these to some kind of temp dir 
-
+// Config says which pages to generate -- todo use JsonPath to match more complicated patterns
+const config = {
+  "pages" : [
+    "Person", 
+    "PersonSnapshot",
+    "RepositoryObject",
+    "DefinedTerm",
+    "File", "Dataset"
+  ]
+}
 
 var crateDir;
 const program = new Command();
@@ -23,7 +32,9 @@ program
   .arguments("<dir>", "Crate directory for input")
   .option("-i, --index.html", "Use index.html as a filename rather than ro-crate-preview.html")
   .action((dir) => { crateDir = dir })
-program.addOption(new Option("-o, --out [out]", "Output to a different directory").default("./src"))
+  program.addOption(new Option("-d, --dist [dist]", "Output files to distribution directory").default("./dist"))
+  program.addOption(new Option("-t --tmp [tmp]", "Output files to distribution directory").default("./tmp"))
+
 
   program.parse(process.argv);
 
@@ -36,13 +47,10 @@ program.addOption(new Option("-o, --out [out]", "Output to a different directory
 
 async function main (program) {
     const crate = new ROCrate(fs.readJSONSync(Path.join(program.crateDir, "ro-crate-metadata.json")), {array: true, link: true})
-    const defaultTypePages = ["Person", "RepositoryObject", "DefinedTerm"]
-
     //const data = new CrateData(crate, {types: types})
     //await data.load();
-
-    const html = new HTML(crate);
-    html.write(program.opts().out);
+    const html = new HTML(crate, config, crateDir, program.opts().tmp, program.opts().dist);
+    html.write(); // to temp path
 }
 
 main(program);
